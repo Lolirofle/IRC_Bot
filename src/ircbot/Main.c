9219e47cbcc168fee01b1a13959ac31687f06f61
Jesper Fridefors <jesper.fridefors@gmail.com>
2014-02-03 23:05:18 +0100

    Provided support for cmd arguments with argtable

--- src/ircbot/Main.c
+++ src/ircbot/Main.c
@@ -13,11 +13,14 @@
 #include "IRCBot.h"
 #include "api/Plugin.h"
 
+#include "argtable2.h"
+
 //TODO: "&&"" to combine commands and maybe `command` to insert a command with output as return value to an argument
 //TODO: Help pages for a list of commands and syntax, explanation, etc.
 //TODO: Command aliases
 
 struct IRCBot bot;
+char* default_channel;
 
 Stringp string_splitted(Stringp str,size_t(*delimiterFunc)(Stringp str),bool(*onSplitFunc)(const char* begin,const char* end)){
 	const char* arg_begin=str.ptr;
@@ -67,7 +70,7 @@ void onMessageFunc(const irc_connection* connection,const irc_message* message){
 		case IRC_MESSAGE_COMMAND_NUMBER:
 			switch(message->command_type_number){
 				case 1:
-					IRCBot_joinChannel(&bot,STRINGCP("#bot",4));
+					IRCBot_joinChannel(&bot,STRINGCP(default_channel,strlen(default_channel)));
 					//IRCBot_joinChannel(&bot,STRINGCP("#toa",4));
 					break;
 				case IRC_MESSAGENO_ERR_NONICKNAMEGIVEN:
@@ -127,7 +130,48 @@ void onMessageFunc(const irc_connection* connection,const irc_message* message){
 	}
 }
 
-int main(){
+int main(int argc, char **argv){
+	
+	int exitcode = 0;
+	//argtable
+	
+	struct arg_str*	nick 	= arg_str1("n", "nick", "<string>",	"nick of the bot");
+	struct arg_str*	server 	= arg_str1(NULL, NULL,  "<string>",	"ip/domain name");
+	struct arg_str* channel = arg_str1("c", "channel", "<string>", "channel to join initially");
+	struct arg_int*	port	= arg_int0("p", "port", "<int>", 	"da port");
+	struct arg_lit*	help 	= arg_lit0("h", "help", "this exakt help");
+	struct arg_end* 	end 	= arg_end(20);
+	void* argtable[] = {nick, server, port, channel, help, end};
+	int nerrors;
+	
+	if (arg_nullcheck(argtable) != 0){
+		/* NULL entries were detected, some allocations must have failed */
+		printf("%s: insufficient memory\n", "toabot");
+		exitcode = 1;
+		goto exit;
+	}
+	
+	
+	port->ival[0] = 6697;
+	
+	nerrors = arg_parse(argc, argv, argtable); 
+	
+	if(help->count > 0){
+		printf("Toabot: The extensible irc bot.\n");
+		printf("https://github.com/Lolirofle/IRC_Bot\n");
+		arg_print_glossary(stdout,argtable,"  %-25s %s\n");
+		exitcode = 0;
+		goto exit;
+	}
+	if(nerrors > 0){
+		arg_print_errors(stdout,end,"toabot");
+		printf("Use --help, you fool");
+		exitcode = 1;
+		goto exit;
+	}
+	//The right arguments are supplied, on to the normal main.
+	default_channel = channel->sval[0];
+	
 	//Top border
 	for(size_t i=IRCBot_signature.length;i>0;--i)
 		putchar('=');
@@ -152,9 +196,9 @@ int main(){
 			fputs("Warning: Failed to initialize modules\n",stderr);
 
 		//Connect to server
-		IRCBot_connect(&bot,Stringcp_from_cstr("server"),1568);
+		IRCBot_connect(&bot,Stringcp_from_cstr(server->sval[0]),port->ival[0]);
 
-		Stringcp name=Stringcp_from_cstr("Toabot");
+		Stringcp name=Stringcp_from_cstr(nick->sval[0]);
 		IRCBot_setNickname(&bot,name);
 		IRCBot_setUsername(&bot,name);
 		IRCBot_setCommandPrefixc(&bot,'!');
@@ -184,6 +228,11 @@ int main(){
 		if(botExit==IRCBOT_EXIT_RESTART)
 			goto Bot;
 	}
+	
+	exitcode = EXIT_SUCCESS;
+	
+	exit:
+		arg_freetable(argtable,sizeof(argtable)/sizeof(argtable[0]));
 
-	return EXIT_SUCCESS;
+	return exitcode;
 }
